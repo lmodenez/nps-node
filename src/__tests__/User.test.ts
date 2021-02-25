e22df598592bc9807713ab6170df55eee292686d
lmodenez <lmodenez98@gmail.com>
2021-02-24 23:04:08 -0300

    implement test with Jest

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -1,9 +1,29 @@
 import request from 'supertest';
 import { app } from '../app';
 
-describe('User', () => {
-  request(app).post('/users').send({
-    email: 'user@example.com',
-    name: 'User Example',
+import createConnection from '../database';
+
+describe('Users', () => {
+  beforeAll(async () => {
+    const connection = await createConnection();
+    await connection.runMigrations();
+  });
+
+  it('Should be able to create a new user', async () => {
+    const response = await request(app).post('/users').send({
+      email: 'user@example.com',
+      name: 'User Example',
+    });
+
+    expect(response.status).toBe(201);
+  });
+
+  it('Should not be able to create a user with exists email', async () => {
+    const response = await request(app).post('/users').send({
+      email: 'user@example.com',
+      name: 'User Example',
+    });
+
+    expect(response.status).toBe(400);
   });
 });
